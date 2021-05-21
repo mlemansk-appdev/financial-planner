7507b4e1651ae8bc54102aba48b1c9459b967d78
mlemansk <mlemansk@chicagobooth.edu>
2021-05-21 19:48:34 +0000

    begining to copy over old controllers

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,59 +1,147 @@
 class LoansController < ApplicationController
   before_action :set_loan, only: %i[ show edit update destroy ]
 
-  # GET /loans or /loans.json
-  def index
-    @loans = Loan.all
+   def index
+    
+      @loans = current_user.loans.all
+      
+      # Initialize Variable
+      @pmt_schedule_chart = [];   
+      @adj_pmt_schedule_chart = []; 
+      @data = []; 
+      @data_adj = []; 
+      @total_interest = 0;
+      @total_adj_interest = 0;
+      @pay_off_period = [];
+      @pay_off_period_adj = [];
+      
+       
+        
+      @loans.each_with_index do |loan , index|
+        pmt_schedule = loan.pay_schedule;  
+        pmt_schedule_adj = loan.adj_pay_schedule(loan.adjustments);
+        @pmt_schedule_chart[index] =  pmt_schedule.map{|t| [ t["pmt_no"], t["beg_balance"] ] };
+        @adj_pmt_schedule_chart[index] = pmt_schedule_adj.map{|t| [ t["pmt_no"], t["beg_balance_adj"] ] };
+        @data << {name: loan.loan_name , data: @pmt_schedule_chart[index]};
+        @data_adj << {name: loan.loan_name , data: @adj_pmt_schedule_chart[index]};
+        @total_interest += pmt_schedule[-1].fetch("cum_int");
+        @total_adj_interest += pmt_schedule_adj[-1].fetch("cum_int_adj");
+        @pay_off_period[index] = pmt_schedule.length;
+        @pay_off_period_adj[index] = pmt_schedule_adj.length;
+      end
+      
+  
+  
+      # Summarize the data for the compare charts
+      total_balance = [];
+      total_periods = [];
+      @total_pmt_schedule_chart = [];
+      
+      @pmt_schedule_chart.each do |pmt|
+        pmt.each_with_index do |period , index|
+          if total_balance[index] == nil
+              total_balance[index] = 0;
+              total_balance[index] += period[1]
+          else
+              total_balance[index] += period[1]
+          end
+          
+          total_periods[index] = period[0]
+          @total_pmt_schedule_chart[index] = [total_periods[index] ,total_balance[index].round(2) ]
+        end
+      end
+      
+      total_adj_balance = [];
+      total_adj_periods = [];
+      @total_adj_pmt_schedule_chart = [];
+      
+      @adj_pmt_schedule_chart.each do |pmt|
+        pmt.each_with_index do |period , index|
+          if total_adj_balance[index] == nil
+              total_adj_balance[index] = 0;
+              total_adj_balance[index] += period[1]
+          else
+              total_adj_balance[index] += period[1]
+          end
+          
+          total_adj_periods[index] = period[0]
+          @total_adj_pmt_schedule_chart[index] = [total_adj_periods[index] ,total_adj_balance[index].round(2) ]
+        end
+      end
+    
+
+    render
   end
 
-  # GET /loans/1 or /loans/1.json
   def show
+    @loan = Loan.find(params.fetch("id_to_display"))
+    
+    @pmt_schedule = @loan.pay_schedule;
+    @pmt_schedule_adj = @loan.adj_pay_schedule(@loan.pmt_adjustments)
+    
+    @pmt_schedule_chart =  @pmt_schedule.map{|t| [ t["pmt_no"], t["beg_balance"] ] };
+    @adj_pmt_schedule_chart = @pmt_schedule_adj.map{|t| [ t["pmt_no"], t["beg_balance_adj"] ] };
+
+    render
   end
 
-  # GET /loans/new
-  def new
+  def new_form
     @loan = Loan.new
+
+    render("loan_templates/new_form.html.erb")
   end
 
-  # GET /loans/1/edit
-  def edit
-  end
+  def create_row
+    @loan = Loan.new
 
-  # POST /loans or /loans.json
-  def create
-    @loan = Loan.new(loan_params)
+    @loan.current_balance = params.fetch("current_balance")
+    @loan.original_amount = params.fetch("original_amount")
+    @loan.interest_rate = params.fetch("interest_rate")
+    @loan.periods_in_year = params.fetch("periods_in_year")
+    @loan.user_id = params.fetch("user_id")
+    @loan.loan_name = params.fetch("loan_name")
+    @loan.monthly_min_payment = params.fetch("monthly_min_payment")
 
-    respond_to do |format|
-      if @loan.save
-        format.html { redirect_to @loan, notice: "Loan was successfully created." }
-        format.json { render :show, status: :created, location: @loan }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @loan.errors, status: :unprocessable_entity }
-      end
+    if @loan.valid?
+      @loan.save
+
+      redirect_to("/loans", :notice => "Loan created successfully.")
+    else
+      render("loan_templates/new_form_with_errors.html.erb")
     end
   end
 
-  # PATCH/PUT /loans/1 or /loans/1.json
-  def update
-    respond_to do |format|
-      if @loan.update(loan_params)
-        format.html { redirect_to @loan, notice: "Loan was successfully updated." }
-        format.json { render :show, status: :ok, location: @loan }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @loan.errors, status: :unprocessable_entity }
-      end
+  def edit_form
+    @loan = Loan.find(params.fetch("prefill_with_id"))
+
+    render("loan_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @loan = Loan.find(params.fetch("id_to_modify"))
+
+    @loan.current_balance = params.fetch("current_balance")
+    @loan.original_amount = params.fetch("original_amount")
+    @loan.interest_rate = params.fetch("interest_rate")
+    @loan.periods_in_year = params.fetch("periods_in_year")
+    @loan.loan_name = params.fetch("loan_name")
+    @loan.monthly_min_payment = params.fetch("monthly_min_payment")
+
+    if @loan.valid?
+      @loan.save
+
+      redirect_to("/loans/#{@loan.id}", :notice => "Loan updated successfully.")
+    else
+      render("loan_templates/edit_form_with_errors.html.erb")
     end
   end
 
-  # DELETE /loans/1 or /loans/1.json
-  def destroy
+  def destroy_row
+    @loan = Loan.find(params.fetch("id_to_remove"))
+
     @loan.destroy
-    respond_to do |format|
-      format.html { redirect_to loans_url, notice: "Loan was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
+    redirect_to("/loans", :notice => "Loan deleted successfully.")
   end
 
   private
--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -1,2 +1,12 @@
 class Adjustment < ApplicationRecord
+    belongs_to :loan
+    belongs_to :user
+    
+    validates :loan_id, :presence => true
+    validates :pmt_adjustment, :numericality => { :greater_than => 0 }
+    validates :pmt_adjustment, :presence => true
+    validates :beg_pay_adj, :numericality => { :greater_than => 0 }
+    validates :beg_pay_adj, :presence => true
+    validates_presence_of :end_pay_adj, :if => lambda { |o| o.payment_occurence == "Between" }
+    
 end
