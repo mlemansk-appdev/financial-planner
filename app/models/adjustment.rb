3b112e736a371f92816c3e48ceb6cf668a5fd73b
mlemansk <mlemansk@chicagobooth.edu>
2021-05-26 02:57:30 +0000

    updated adjustment model validations

--- app/models/adjustment.rb
+++ app/models/adjustment.rb
@@ -17,11 +17,15 @@ class Adjustment < ApplicationRecord
     belongs_to :loan
     belongs_to :user
     
-    validates :loan_id, :presence => true
     validates :pmt_adjustment, :numericality => { :greater_than => 0 }
     validates :pmt_adjustment, :presence => true
     validates :beg_pay_adj, :numericality => { :greater_than => 0 }
     validates :beg_pay_adj, :presence => true
-    validates_presence_of :end_pay_adj, :if => lambda { |o| o.payment_occurrence == "Between" }
+    #validates_presence_of :end_pay_adj, :if => lambda { |o| o.payment_occurrence == "Between" }
+    validates :end_pay_adj, :presence => true, :if => :payment_occurrence_between
+
+  def payment_occurrence_between
+    payment_occurrence == 'Between'
+  end
     
 end
